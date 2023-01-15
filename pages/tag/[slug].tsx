5e6bea0dc2f6af994adde0bd4f7cb4ead8661835
Kevin Wynn <kevinwynn@Kevins-MacBook-Pro.local>
2023-01-15 09:57:07 -0600

    fallback blocking

--- pages/tag/[slug].tsx
+++ pages/tag/[slug].tsx
@@ -21,7 +21,7 @@ export async function getStaticPaths() {
 
   return {
     paths: paths.map((slug: string) => ({ params: { slug } })),
-    fallback: process.env.NODE_ENV === "development" ? true : false,
+    fallback: process.env.NODE_ENV === "development" ? true : "blocking",
   };
 }
 
