c44089436e5d0c0ea15fcd7e6857aac9065c5ef2
Kevin Wynn <kevinwynn@Kevins-MacBook-Pro.local>
2024-03-26 11:52:07 -0500

    v3 waco shop opening

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,11 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import preact from "@astrojs/preact";
+
 import tailwind from "@astrojs/tailwind";
 import cloudflare from "@astrojs/cloudflare";
-import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://robo-house.com",
+  integrations: [tailwind()],
   output: "server",
   adapter: cloudflare(),
-  devToolbar: {
-    enabled: false,
-  },
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind(),
-    preact(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
 });
--- src/consts.ts
+++
@@ -1,2 +0,0 @@
-export const SITE_TITLE = "Robo House";
-export const SITE_DESCRIPTION = "Here we are.";
