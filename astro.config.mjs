e1d37cff7e8d9c57189ddac6d266ad33af48b5e6
Kevin Wynn <kevinwynn@Kevins-MacBook-Pro.local>
2024-03-26 20:26:49 -0500

    add google analytics tracker

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,20 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import cloudflare from "@astrojs/cloudflare";
+import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind({
       applyBaseStyles: false,
+    }),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
     }),
   ],
   output: "server",
